7e7518247dc9be5cbf11d38a58be08550c6fc9d9
CRF <crf1989>
2016-08-23 17:44:37 +0800

    minor edit

--- main.cpp
+++ main.cpp
@@ -4,35 +4,7 @@
 #include "density_of_states.hpp"
 #include "green_function.hpp"
 #include "dmft.hpp"
-//#include "magnetization.hpp"
 using namespace std;
-
-// double kubo_greenwood (green_function& G, double t, double u, double T)
-// {
-//   int L = 40;
-//   double h = 2*M_PI/L;
-//   double result = 0;
-
-//   for (int i = 0; i < G.size(); ++i)
-//     {
-//       if (fabs(G.sigma(i).imag()) < 1e-4)
-// 	continue;
-//       if (G.omega(i) > 0)
-// 	continue;
-//       for (int x = 0; x < L; ++x)
-// 	for (int y = 0; y < L; ++y)
-// 	  for (int z = 0; z < L; ++z)
-// 	    {
-// 	      double epsilon = 2*t*(cos(-M_PI+x*h) + 
-// 				    cos(-M_PI+y*h) + cos(-M_PI+z*h));
-// 	      double ImG = (1./(G.omega(i)-G.sigma(i)-epsilon)).imag();
-// 	      double v = -sin(-M_PI+x*h);
-// 	      result += exp((u-G.omega(i))/T)/T*v*v*ImG*ImG;
-// 	    }
-//     }
-//   return result/(2*M_PI*2*M_PI*2*M_PI)*h*h*h;
-// }
-  
 
 int main (int argc, char* argv[])
 {
